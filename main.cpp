99d86415a9eab3b670b6f131fd46a2ddd0ccac6b
argb <argbinf@gmail.com>
2020-04-25 15:00:16 +0200

    Sockets

--- main.cpp
+++ main.cpp
@@ -5,5 +5,19 @@ using namespace std;
 int main()
 {
   cout << "NJuan!" << endl;
+
+  //Check an open port
+  //sf::TcpSocket socket;
+
+  //bool open = (socket.connect(sf::IpAddress(address), port) == sf::Socket::Done);
+  //socket.disconnect();
+
+
+
+
+  //List ports from localhost
+
+
+
   return 0;
 }
