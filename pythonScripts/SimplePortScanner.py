7dd99ed7f8ae7531aa2c1b0bdc75c2d9a3698a1b
argb10 <argbinf@gmail.com>
2020-04-26 05:42:42 -0400

    Folder Config

---
+++ pythonScripts/SimplePortScanner.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 11:17:27 2020
+
+@author: argb
+"""
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server = 'pythonprogramming.net'
+
+def pscan(port):
+    try:
+        s.connect((server, port))
+        return True
+    except:
+        return False
+
+for x in range(80,445):
+    if pscan(x):
+        print('Port ',x,' is open')
