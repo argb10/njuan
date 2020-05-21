e98cba9d7fafc18a4035c5dd0fc05cdea054f255
argb10 <argbinf@gmail.com>
2020-05-21 05:01:59 -0400

    All ports scanning almost fixed (sometimes some ports up to 35000 seem open but they are not) and a little bit of cleaning code and add user input

--- main.cpp
+++ main.cpp
@@ -3,73 +3,87 @@
 #include <arpa/inet.h> 
 #include <unistd.h> 
 #include <string.h> 
+
 int create_socket_tcp(){
 	int sock = 0;
-    //Socket Creation: AF_INET=IPV4, SOCK_STREAM=safe two way comunication, 0=standard protocol to the sock_stream specified
+    //Socket Creation: AF_INET=IPV4, SOCK_STREAM=safe two way comunication(tcp), 0=standard protocol to the sock_stream specified
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
-        printf("\n Njuan: Socket creation error \n"); 
+        printf("\n NjuanAlert: Socket creation error \n"); 
         return -1; 
     }
     else 
     	return sock;
 }
-bool port_is_open(char const *TARGET, int PORT, int sock){
+bool port_is_open(char const *TARGET, int PORT){
+	int sock = create_socket_tcp();
 	struct sockaddr_in serv_addr;
-   	//Specifies protocol to try the connection
+
+   	//Set Ipv4
     serv_addr.sin_family = AF_INET; 
 
-    // Convert IPv4 and IPv6 addresses from text to binary form 
+    //Set port
+    serv_addr.sin_port = htons(PORT); 
+
+    // Convert IPv4 addresses from text to binary
     if(inet_pton(AF_INET, TARGET, &serv_addr.sin_addr)<=0)  
     { 
-        printf("\nInvalid address/ Address not supported \n"); 
+        printf("\nNjuanAlert: Invalid address / Address not supported \n"); 
         return -1; 
-    } 
+    }
    
-    serv_addr.sin_port = htons(PORT); 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        //printf("\nConnection Failed \n"); 
-        return -1; 
-    }
+    //Try connect
+    int status = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    //Close socket so we can continue scanning
+    close(sock);
+
+    //Shoulf I do the sleep here?----------------
+    //sleep(0.0001);
+
+    if (status < 0)
+        return -1; //Connection Failed
     else
-    {
     	return 0;
-    }
 }
 //nmap <ip>
 void check_from_1_to_1000(char const *TARGET){
 
-	int sock = create_socket_tcp();
 	for(int port=1 ; port < 1000 ; port++)
-		if(port_is_open(TARGET, port, sock) == 0)
+		if(port_is_open(TARGET, port) == 0)
 			printf("Port %d: Open\n", port);
 }
+
 //nmap -p- <ip> 
 void check_from_1_to_65535(char const *TARGET){
-
-	int sock = create_socket_tcp();	
-	for(int port=1 ; port < 65535 ; port++){
-        //printf("Scaning %d\n",port);
-		if(port_is_open(TARGET, port, sock) == 0)
+	
+	for(int port=1 ; port < 65535 ; port++)
+		if(port_is_open(TARGET, port) == 0)
 			printf("Port %d: Open\n", port);
-        sleep(0.5);
-    }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-  	printf("NJuan!\n");
-	
-    /*Check my postgres at 5432
-    int sock = create_socket_tcp();
-    if(port_is_open("127.0.0.1", 5432, sock) == 0)
-        printf("Port %d: Open\n", 5432);
-    */
-	//List ports from localhost
-	//check_from_1_to_1000("127.0.0.1"); 
-    printf("--------------------------------\n");
-  	check_from_1_to_65535("127.0.0.1"); 
+    if (argc < 2 || argc > 3)
+    {
+        printf ("Please enter the server IP address and range of ports to be scanned\n");
+        printf ("USAGE: %s <IP> (-p-)\n", argv[0]);
+        return(1);
+    }
+    
+    char IP[16] = {0};
+    strcpy(IP, argv[1]); // Copy the IP
 
+    char ports[3] = {0};
+    if (argc == 3){
+   		strcpy(ports, argv[2]); // Copy the parameter
+	}
+ 	
+  	printf("NJuan! :)\n");
+	
+	if( strcmp(ports, "-p-") == 0)
+		check_from_1_to_65535("127.0.0.1");
+	else
+		check_from_1_to_1000("127.0.0.1");
   	return 0;
 }
